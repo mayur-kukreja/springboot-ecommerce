012aa685e62217961a3374bdafea4affc26a39c2
mayur-kukreja <mayurkukreja222@gmail.com>
2025-10-19 21:51:43 +0530

    added cookies logger

--- src/main/java/com/sb/eccomerce/security/jwt/JwtUtils.java
+++ src/main/java/com/sb/eccomerce/security/jwt/JwtUtils.java
@@ -38,10 +38,10 @@ public class JwtUtils {
     public String getJwtFromCookies(HttpServletRequest request){
         Cookie cookie = WebUtils.getCookie(request, jwtCookie);
         if (cookie != null){
-            System.out.println("cookies found");
+            logger.error("cookies found");
             return cookie.getValue();
         } else {
-            System.out.println("cookies not found");
+            logger.error("cookies not found");
             return null;
         }
 
