befe896a21a615943a8e8821e0647e2041c69e1e
Mayur Kukreja <157275651+mayur-kukreja@users.noreply.github.com>
2025-10-20 12:56:39 +0530

    Update JwtUtils.java

--- src/main/java/com/sb/eccomerce/security/jwt/JwtUtils.java
+++ src/main/java/com/sb/eccomerce/security/jwt/JwtUtils.java
@@ -52,7 +52,7 @@ public class JwtUtils {
         return ResponseCookie.from(jwtCookie, jwt).path("/api")
                 .maxAge(24*60*60)
                 .httpOnly(false)
-                .secure(false)
+                .secure(true)
                 .sameSite("None")  
                 .build();
     }
